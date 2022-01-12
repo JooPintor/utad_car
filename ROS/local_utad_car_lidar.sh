4aa955745213a092bd7e3223b13dc4c46bc8375b
JooPintor <89998899+JooPintor@users.noreply.github.com>
2022-01-12 15:41:01 +0000

    Update local_utad_car_lidar.sh

--- ROS/local_utad_car_lidar.sh
+++ ROS/local_utad_car_lidar.sh
@@ -1,7 +1,8 @@
+# Inicia o modulo 'utad_car.launch' com servidor de ROS local
 
-#export ROS_MASTER_URI=http://rpi4:11311
 export ROS_MASTER_URI=http://localhost:11311
 
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws
+
 roslaunch utad_car_bringup utad_car.launch
