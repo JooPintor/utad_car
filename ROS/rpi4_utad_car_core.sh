6c6d8eee568be43f878fe62b32d4766adaef2b32
JooPintor <89998899+JooPintor@users.noreply.github.com>
2022-01-12 15:47:20 +0000

    Update and rename remote_utad_car_core.sh to rpi4_utad_car_core.sh

--- ROS/rpi4_utad_car_core.sh
+++ ROS/rpi4_utad_car_core.sh
@@ -1,7 +1,8 @@
+# Inicia o modulo 'utad_car_core.launch' com servidor de ROS no Raspberry PI 4
 
 export ROS_MASTER_URI=http://rpi4:11311
-#export ROS_MASTER_URI=http://localhost:11311
 
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws
+
 roslaunch utad_car_core utad_car_core.launch
