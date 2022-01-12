3b153a12030b207d15f4649371e4feba45decec8
JooPintor <89998899+JooPintor@users.noreply.github.com>
2022-01-12 15:56:49 +0000

    Update rpi4_rs2_camera.sh

--- ROS/rpi4_rs2_camera.sh
+++ ROS/rpi4_rs2_camera.sh
@@ -1,9 +1,12 @@
+# Inicia o modulo 'realsense2_camera' com servidor de ROS no Raspberry PI 4
+# Diferentes opções de arranque -> Apenas a linha com roslaunch que não começa por # é executada
 
 export ROS_MASTER_URI=http://rpi4.local:11311
-#export ROS_MASTER_URI=http://localhost:11311
+
 
 source ~/catkin_ws/devel/setup.bash
 cd ~/catkin_ws
+
 roslaunch realsense2_camera rs_camera.launch enable_sync:=true
 #roslaunch realsense2_camera rs_camera.launch filters:=pointcloud enable_infra:=true depth_width:=424 infra_width:=424 color_width:=424 depth_height:=240 infra_height:=240 color_height:=240 depth_fps:=30 color_fps:=30 infra_fps:=30
 #roslaunch realsense2_camera rs_d435_camera_with_model.launch
